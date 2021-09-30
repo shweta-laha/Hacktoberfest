3ac6140b4a03bf85af92dc007cde1d5a993873ae
Shweta <65334243+shweta-laha@users.noreply.github.com>
2021-09-30 12:55:47 +0530

    Add files via upload

---
+++ strange.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class strange{    
+ public static void main(String args[])
+ {
+	 Scanner input= new Scanner(System.in);
+	 int n=input.nextInt();
+	int max_prime=largestPrimeFactor(n);
+	if(max_prime>Math.sqrt(n)) {
+		System.out.print("Strange");
+	}
+	else
+	{
+		System.out.print("Not Strange");
+	}
+	
+ }
+public static int largestPrimeFactor(int n) {  
+	  if (n <= 1) {  
+	   return 0;  
+	  } else {  
+	   int div = 2;  
+	   while (div < n) {  
+	    if (n % div != 0) {  
+	     div++;  
+	    } else {  
+	     n = n / div;  
+	     div = 2;  
+	    }  
+	   }  
+	   return n;  
+	  }  
+	  
+	}   
+}
