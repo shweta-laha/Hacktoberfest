bd900166801912efd881f1161f64a2704113349f
Anand <73114463+Anand4756@users.noreply.github.com>
2021-10-07 16:40:39 +0530

    1d array in java

--- Java Files/Array.java
+++ Java Files/Array.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
-//hackerrank 1d java solution
+
 public class Array {
 
     public static void main(String[] args) {
 	   
         Scanner scan = new Scanner(System.in);
+         int n = scan.nextInt();
          int[] a = new int[n];
         for(int i = 0; i < n; i++){
         a[i]=scan.nextInt();
