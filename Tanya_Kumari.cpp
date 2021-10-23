3e91f732c73387cdc14f8056817eeb3614dfb168
tk3103 <77692937+tk3103@users.noreply.github.com>
2021-10-23 22:54:40 +0530

    Create Tanya_Kumari.cpp

---
+++ Tanya_Kumari.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+     
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+
+    return 0;
+}
