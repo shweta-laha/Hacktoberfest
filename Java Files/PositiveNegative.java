6fd320a1289e48e41529fa48d260adf3aded124e
tk3103 <77692937+tk3103@users.noreply.github.com>
2021-10-28 00:45:27 +0530

    Create PositiveNegative.java

---
+++ Java Files/PositiveNegative.java
@@ -0,0 +1,19 @@
+public class PositiveNegative {
+
+    public static void main(String[] args) {
+
+        double number = 12.3;
+
+        
+        if (number < 0.0)
+            System.out.println(number + " is a negative number.");
+
+        
+        else if ( number > 0.0)
+            System.out.println(number + " is a positive number.");
+
+        
+        else
+            System.out.println(number + " is 0.");
+    }
+}
